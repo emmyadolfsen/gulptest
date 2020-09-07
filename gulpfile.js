321c162e1a724a9cf7faa76084486cfcbf38b7a2
Emmy Adolfsen <emmy.adolfsen@gmail.com>
2020-09-07 14:16:55 +0200

    Added csstask - cleanCss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const { src, dest, watch, series, parallel } = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify-es").default;
+const cleanCSS = require('gulp-clean-css');
+
+
 // Sökvägar
 const files = {
     htmlPath: "src/**/*.html",
@@ -13,13 +16,14 @@ function copyHTML() {
     return src(files.htmlPath)
         .pipe(dest('pub'));
 }
+/*
 // Kopiera CSS-filer
 function copyCSS() {
     return src(files.cssPath)
         .pipe(dest('pub'));
 }
-
-// sammanslå
+*/
+// Sammanslå
 function jsTask() {
     return src(files.jsPath)
         .pipe(concat('main.js'))
@@ -27,15 +31,21 @@ function jsTask() {
         .pipe(dest('pub/js'));
 }
 
+function cssTask() {
+    return src(files.cssPath)
+        .pipe(concat('stylesheet.css'))
+        .pipe(cleanCSS())
+        .pipe(dest('pub/css'));
+}
+
 // Watcher
 function watchTask() {
-    watch([files.htmlPath, files.jsPath],
-        parallel(copyHTML, jsTask)
+    watch([files.htmlPath, files.jsPath, files.cssPath],
+        parallel(copyHTML, cssTask, jsTask)
     );
 }
 
-
 exports.default = series(
-    parallel(copyHTML, copyCSS, jsTask),
+    parallel(copyHTML, cssTask, jsTask),
     watchTask
 );
